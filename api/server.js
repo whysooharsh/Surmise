702b1b2705807dcac65c55c5268fd2f4de41af9c
harsh <harsh.sharma8986@gmail.com>
2026-06-24 03:32:02 +0530

    build fix

--- api/server.js
+++ api/server.js
@@ -131,9 +131,9 @@ app.use((err, req, res, next) => {
 });
 
 if (process.env.NODE_ENV === 'production' && !process.env.VERCEL) {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  app.use(express.static(path.join(__dirname, '../dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 }
 
